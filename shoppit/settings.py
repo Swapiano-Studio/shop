1bdb74b5292aa8e18a39f0538ad58d7e73ec29e6
Nova Herawan <kadeksananda@gmail.com>
2025-05-26 13:38:41 +0800

    vercel

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
